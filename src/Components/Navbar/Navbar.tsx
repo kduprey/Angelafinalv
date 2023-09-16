0b6dc2e25416ca871901c8a60de31342d6183968
Kenton Duprey <kentonduprey@icloud.com>
2023-09-15 22:00:58 -0400

    Moved Navbar into separate dir

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import "./Navbar.css";
+import AM from "/AM.png";
+
+export const Navbar = () => {
+	const handleClick = () => {
+		// console.log("Clicked");
+	};
+	return (
+		<nav className="relative h-32 w-32">
+			<div className="logo">
+				<Image src={AM} alt="Logo" width={"41.792"} height={"40"} />
+			</div>
+			<div className="absolute top-0 right-0 h-1">
+				<Link href="mailto:hello@angelatmcleod.com" className="contact">
+					Contact
+				</Link>
+			</div>
+		</nav>
+	);
+};
