1716c9782f2857d216c88779c3c6603e54f9d6dd
Kenton Duprey <kentonduprey@icloud.com>
2023-09-15 22:01:17 -0400

    Moved Connect into separate dir

---
+++ src/Components/Collaborate/Connect/Connect.tsx
@@ -0,0 +1,11 @@
+import "./Connect.css";
+
+export const Connect = () => {
+	return (
+		<div className="connect-container">
+			<div className="header-container">
+				<h1>Get in Touch</h1>
+			</div>
+		</div>
+	);
+};
