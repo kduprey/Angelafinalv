aeb09bb6c9c82ef52da77747971a4c572027bd79
Kenton Duprey <kentonduprey@icloud.com>
2023-09-17 13:21:21 -0400

    Updating eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-  extends: "next/core-web-vitals",
+	extends: [
+		"next/core-web-vitals",
+		"prettier",
+		"plugin:tailwindcss/recommended",
+		"plugin:@typescript-eslint/recommended",
+	],
+	plugins: ["tailwindcss", "unused-imports"],
+	rules: {
+		"tailwindcss/no-custom-classname": "off",
+	},
+	settings: {
+		tailwindcss: {
+			config: "tailwind.config.js",
+		},
+	},
 };
