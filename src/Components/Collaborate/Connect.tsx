9b8b674ddc43f347e8cb44068695cd6ac6713c0d
DanielDecembre <96702604+DanielDecembre@users.noreply.github.com>
2023-09-18 23:59:07 -0400

    launch ph1

--- src/Components/Collaborate/Connect.tsx
+++ src/Components/Collaborate/Connect.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import "./Connect.css"
+import React from "react";
+import "./Connect.css";
 
 function Connect() {
   return (
     <div className="connect-container">
       <div className="header-container">
-        <h1>Get in Touch</h1>
+        <h1>Get in touch</h1>
       </div>
-
+      <div className="info-container">
+      <p>Based in Silicon Valley, working remotely with global clients.</p>
+      <p>+1.515.208.2306</p>
       </div>
-  )
+    </div>
+  );
 }
 
-export default Connect
-
+export default Connect;
